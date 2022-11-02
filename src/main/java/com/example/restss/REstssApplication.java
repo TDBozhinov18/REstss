c77a44ba7aed2f69941eb25b392db2528bad7390
TDBozhinov18 <TDBozhinov18@codingburgas.bg>
2022-11-02 15:32:58 +0200

    Initial commit

---
+++ src/main/java/com/example/restss/REstssApplication.java
@@ -0,0 +1,13 @@
+package com.example.restss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class REstssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(REstssApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restss/REstssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class REstssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
