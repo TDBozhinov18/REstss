31d832bf9da086ed0c50d5e642e6eed20d4e7314
TDBozhinov18 <TDBozhinov18@codingburgas.bg>
2022-11-02 15:41:05 +0200

    finish

---
+++ src/main/java/com/example/restss/Movie.java
@@ -0,0 +1,30 @@
+package com.example.restss;
+
+public class Movie {
+    int id;
+    String name;
+    double rating;
+
+    public Movie(int id, String name, double rating) {
+        this.id = id;
+        this.name = name;
+        this.rating = rating;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie [id=" + id + ", name=" + name + ", rating=" + rating + "]";
+    }
+}
